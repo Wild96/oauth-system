078891dd691c82bf5b5fa2ec5b198c2d1dd76816
sai prasath <saiprasath801@gmail.com>
2018-05-14 14:04:24 +0530

    changed stufff in controllers

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -5,24 +5,22 @@ var moment = require('moment');
 module.exports = {
     getuser: async function(request,response){
         try{
-            //console.log("request.body",request.body.token);
-            //check if token has expired then give message sayin that it has expired in the  console.
             var user_details = await token.findOne({token: request.body.token});
-         //   console.log("user details:",user_details);
             var creation_time = user_details.creation_time;
-          //  console.log("creation time",creation_time);
             var expiry_time = user_details.expiry_time;
             var current_time = new moment().format("HHmm");
-          //  console.log("current time:",current_time);
             if(current_time > expiry_time){
                 console.log("token expired");
                 var remove_token = await token.remove({token:request.body.token});
                 console.log("token removed request a new one");
-            
             }
-            console.log("username",user_details.Username);
-            console.log("password:",user_details.Password);
-            return response.status(200);
+            else{
+                console.log("user id ",user_details.UserId);
+                console.log("username",user_details.Username);
+                
+                return response.status(200);
+            }
+           
         }
         catch(e){
             return response.status(500).send({ error:true,message:e.toString() });
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,11 +4,11 @@ const user = require('../models/user');
 module.exports = {
     registeruser : async function (request,response,next){
         try{
-            console.log("request:",request.body);
-            console.log("username",request.body.Username);
-            console.log("password",request.body.Password);
+           // console.log("request:",request.body);
+            //console.log("username",request.body.Username);
+            //console.log("password",request.body.Password);
             let newUser = new user({Username:request.body.Username,Password:request.body.Password});
-            console.log("new user", newUser);
+            //console.log("new user", newUser);
             await newUser.save();
             return response.status(200).send({error:false,message:"user created successfullly"});
         }
